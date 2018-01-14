2554cdf5b10e9cdf959ce4f7842a5486eef8db81
GbeTech <gbetech@gmail.com>
2018-01-14 20:03:07 +0200

    test_matrix_tuple_columns_stop [:, :2] passes

--- tests/matrix_tests/tuple_tests/test_matrix_tuple_columns_stop.py
+++ tests/matrix_tests/tuple_tests/test_matrix_tuple_columns_stop.py
@@ -1,19 +1,18 @@
 from ...boilerplate import Boilerplate
-from src.matrix import Vector
 
 bp = Boilerplate()
 
 
 def test_tuple_colon_1():
 	bp.basic_test(res=bp.m[:, :2],
-	                 expected=[['A', 'E', 'J'],
-	                           ['B', 'F', 'K']],
-	                 res_type=list)
+	              expected=[['A', 'E', 'J'],
+	                        ['B', 'F', 'K']],
+	              res_type=list)
 
 
 def test_tuple_colon_rev1():
 	bp.basic_test(res=bp.m[:, :-2],
-	                 expected=[['A', 'E', 'J'],
-	                           ['B', 'F', 'K'],
-	                           ['C', 'G', 'L']],
-	                 res_type=list)
+	              expected=[['A', 'E', 'J'],
+	                        ['B', 'F', 'K'],
+	                        ['C', 'G', 'L']],
+	              res_type=list)
