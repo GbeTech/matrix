bc226a3b0f11a9bede209db3cb08cfe95fb8d7ad
GbeTech <gbetech@gmail.com>
2018-01-19 05:43:19 +0200

    INDEX GETTERS / SLICERS WORK AND ROBUST

--- tests/matrix_tests/combined_tests/test_matrix_combined.py
+++ tests/matrix_tests/combined_tests/test_matrix_combined.py
@@ -21,8 +21,9 @@ def test_03():
 	              expected=['K', 'L', 'M', None],
 	              res_type=list)
 
-# def test_rev1_colon_rev4_colon_rev2():
-# 	bp.basic_test(res=bp.m[-1::-2, -2::-2],
-# 	              expected=[['M', 'K'],
-# 	                        ['D', 'B']],
-# 	              res_type=list)
+
+def test_rev1_colon_rev4_colon_rev2():
+	bp.basic_test(res=bp.m[-1::-2, -2::-2],
+	              expected=[['M', 'K'],
+	                        ['D', 'B']],
+	              res_type=list)
